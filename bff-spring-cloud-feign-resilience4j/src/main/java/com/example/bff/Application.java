f84f30e1400a84f94d58230270e9b416700babc0
arturdgmr <artur_dgmr@hotmail.com>
2021-08-28 00:03:59 -0300

    Ajusta nome da classe de inicialização da aplicação

--- bff-spring-cloud-feign-resilience4j/src/main/java/com/example/bff/Application.java
+++ bff-spring-cloud-feign-resilience4j/src/main/java/com/example/bff/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BffSpringCloudFeignResilience4jApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BffSpringCloudFeignResilience4jApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
