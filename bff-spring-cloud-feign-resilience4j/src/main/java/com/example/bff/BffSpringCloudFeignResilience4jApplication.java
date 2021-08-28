c2a68e824b22951f3f3fb29713c462e2feeb9e27
arturdgmr <artur_dgmr@hotmail.com>
2021-08-27 22:32:35 -0300

    Primeiro commit

---
+++ bff-spring-cloud-feign-resilience4j/src/main/java/com/example/bff/BffSpringCloudFeignResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSpringCloudFeignResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffSpringCloudFeignResilience4jApplication.class, args);
+	}
+
+}
