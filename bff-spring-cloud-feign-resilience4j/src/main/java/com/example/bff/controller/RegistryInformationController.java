fb293b3136fb45b87e607e715261540d10ed8a19
arturdgmr <artur_dgmr@hotmail.com>
2021-08-28 17:18:33 -0300

    Adiciona retry do resilience4j

--- bff-spring-cloud-feign-resilience4j/src/main/java/com/example/bff/controller/RegistryInformationController.java
+++ bff-spring-cloud-feign-resilience4j/src/main/java/com/example/bff/controller/RegistryInformationController.java
@@ -1,15 +1,25 @@
 package com.example.bff.controller;
 
+import com.example.bff.client.CustomerProfile;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = {"/registryinformation"})
 public class RegistryInformationController {
 
+    private CustomerProfile customerProfile;
+
     @GetMapping
+    @Retry(name = "customer-profile-api")
     public String getTeste(){
-        return "teste";
+        log.info("Request to customer profile api is received!");
+        return customerProfile.getRegistryInformation();
     }
 }
