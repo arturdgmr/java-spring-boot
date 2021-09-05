18bea959d861f8727fba71cbdcf81adb820bc558
arturdgmr <artur_dgmr@hotmail.com>
2021-09-05 00:14:50 -0300

    Adiciona estrutura do kafka e o consumidor

---
+++ spring-kafka/src/main/java/br/com/example/kafka/config/KafkaConfiguration.java
@@ -0,0 +1,66 @@
+package br.com.example.kafka.config;
+
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
+
+import java.util.Properties;
+
+@AllArgsConstructor
+@Configuration
+public class KafkaConfiguration {
+    private KafkaConfigData config;
+    private static final String ACKS_ALL_CONSTANT = "all";
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory(senderProps());
+    }
+
+    private Properties senderProps() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        props.put(ProducerConfig.ACKS_CONFIG, ACKS_ALL_CONSTANT);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
+        return new KafkaTemplate(producerFactory);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory(consumerProps());
+    }
+
+    private Properties consumerProps() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, config.getEventsConsumerGroup());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, config.getEventsAutoOffsetResetConfig());
+        return props;
+    }
+}
---
+++ spring-kafka/src/main/java/br/com/example/kafka/consumers/CostumerRegistrationConsumer.java
@@ -0,0 +1,18 @@
+package br.com.example.kafka.consumers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CostumerRegistrationConsumer {
+
+    @KafkaListener(topics = "${kafka.listener.topics.finish-registration}", groupId = "${kafka.listener.events-consumer-group}")
+    public void consumer(@Payload String message, Acknowledgment ack){
+        log.info("M=consumer, message={}", message);
+        ack.acknowledge();
+    }
+}
