9a9a3ca6658935c84adff310f1c8b08258cff6a0
arturdgmr <artur_dgmr@hotmail.com>
2022-02-21 20:21:58 -0300

    Ajusta descrição da classe usada para cada cargo

--- java-oo-solid/src/main/java/com/java/oo/solid/singleresponsiblity/solucao/CalculadoraDeSalario.java
+++ java-oo-solid/src/main/java/com/java/oo/solid/singleresponsiblity/solucao/CalculadoraDeSalario.java
@@ -6,11 +6,11 @@ public class CalculadoraDeSalario {
         /** DEV */
         Funcionario funcionarioDev = new Funcionario(CargoEnum.DESENVOLVEDOR, 3000);
         System.out.println("Regra de calculo para Dev: " + CargoEnum.DESENVOLVEDOR.getRegraDeCalculo().calcula(funcionarioDev));
-        System.out.println("Regra de calculo para Dev: " + CargoEnum.DESENVOLVEDOR.getRegraDeCalculo().getClass().getSimpleName());
+        System.out.println("Qual é a regra de calculo para Dev: " + CargoEnum.DESENVOLVEDOR.getRegraDeCalculo().getClass().getSimpleName());
 
         /** DBA */
         Funcionario funcionarioDBA = new Funcionario(CargoEnum.DBA, 5000);
         System.out.println("Regra de calculo para DBA: " + CargoEnum.DBA.getRegraDeCalculo().calcula(funcionarioDBA));
-        System.out.println("Regra de calculo para DBA: " + CargoEnum.DBA.getRegraDeCalculo().getClass().getSimpleName());
+        System.out.println("Qual é a regra de calculo para DBA: " + CargoEnum.DBA.getRegraDeCalculo().getClass().getSimpleName());
     }
 }
